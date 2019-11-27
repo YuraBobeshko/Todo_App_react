95c794881821de9ebdabc53ec48a3c7cbbb8ec29
YuraBobeshko <gorobech55@gmail.com>
2019-11-27 14:11:13 +0200

    Updates

---
+++ precache-manifest.ba61efbfb8508aaaad2e735abc44234c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3a2385ce2912eb87c0114ebc72d24cd",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "45e9aec1991a82748b33",
+    "url": "/react_todo-app/static/css/main.fed7f93c.chunk.css"
+  },
+  {
+    "revision": "881ac9a3c94bef61c917",
+    "url": "/react_todo-app/static/js/2.b65d8632.chunk.js"
+  },
+  {
+    "revision": "45e9aec1991a82748b33",
+    "url": "/react_todo-app/static/js/main.08ccc7ac.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
